a2eb5ebc7edcf18a55a91fd12ad242d1b27509cb
shahnoorgit <shahnoormujawar@gmail.com>
2024-04-19 19:53:13 +0530

    Catolog started

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,12 @@ export default function Home() {
   return (
     <main className=" overflow-hidden ">
       <Hero />
+      <div className=" mt-12 padding-y max-width padding-x" id="discover">
+        <div className=" home__text-container">
+          <h1 className=" font-extrabold text-4xl">Car Catalogue</h1>
+          <p>Explore Cars Of your Locality</p>
+        </div>
+      </div>
     </main>
   );
 }
